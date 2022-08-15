8e95b79b9fbffc95a32f9b19f7bad6f37e4b0c55
Hasan Özdişçi <hasanozdisci@gmail.com>
2022-08-15 19:14:08 +0300

    template engine ejs added project

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-import express from "express"
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send("INDEX SAYFASI 2")
+//ejs template engine
+app.set('view engine', 'ejs')
+
+//static files middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render('index');
 });
 
+app.get("/about", (req, res) => {
+    res.render('about');
+  });
+
 app.listen(port, () => {
-    console.log(`Application running on port: ${port}`)
+  console.log(`Application running on port: ${port}`);
 });
